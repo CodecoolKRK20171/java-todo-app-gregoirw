47b2786b0db0d450c6d79ebe4b98c811d83a535e
Grzegorz Winiarek <g.d.winiarek@gmail.com>
2017-08-18 14:12:31 +0200

    I hate commits

--- App.java
+++ App.java
@@ -1,9 +1,63 @@
-/**
- * Class representing the TodoApp application.
- * It is the main entry point for this program.
- */
+import java.util.Scanner;
+
 public class App{
   public static void main(String[] args) {
+        String showMenu = "show, mark, add, archive, exit";
 
-  }
+        TodoList ToDoList = new TodoList();
+
+        while (true) {
+            System.out.println(showMenu);
+            System.out.println("Please type an option: ");
+            Scanner scan = new Scanner(System.in);
+            String answer = scan.nextLine().toUpperCase();
+
+            if(answer.equals("SHOW")) {
+                showAllItems(ToDoList);
+
+            } else if(answer.equals("ADD")) {
+                System.out.println("Please provide new item: ");
+
+                String name = scan.nextLine().toUpperCase();
+                TodoItem ToDoItem = new TodoItem(name);
+                addNewItem(ToDoItem, ToDoList);
+
+            } else if(answer.equals("MARK")){
+                showAllItems(ToDoList);
+                System.out.println("Please type item you want to mark: ");
+                String name = scan.nextLine().toUpperCase();
+                markItem(name, ToDoList);
+
+            } else if(answer.equals("ARCHIVE")){
+                archive(ToDoList);
+
+            } else if(answer.equals("EXIT")){
+                System.exit(0);
+            }
+
+
+            }
+        }
+
+
+
+    public static void showAllItems(TodoList ToDoList) {
+        ToDoList.showTasks();
+    }
+
+    public static void addNewItem(TodoItem name, TodoList ToDoList) {
+        ToDoList.addToList(name);
+    }
+
+    public static void markItem(String name, TodoList ToDoList) {
+        for (TodoItem item : ToDoList.getItems()) {
+            if (item.getName().equals(name)) {
+                item.isCompleted = true;
+            }
+        }
+    }
+
+    public static void archive(TodoList ToDoList) {
+        ToDoList.deleteItem();
+    }
 }
--- TodoItem.java
+++ TodoItem.java
@@ -1,7 +1,19 @@
-/**
- * Class representing a single todo item.
- * 
- */
+import java.util.ArrayList;
+
 public class TodoItem{
+    private String name;
+    Boolean isCompleted = false;
+
+    public TodoItem(String name){
+        this.name = name;
+
+        }
+    public void getCompleted(TodoItem this){
+        this.isCompleted = true;
+    }
+
+    public String getName(){
+        return this.name;
+    }
 
 }
